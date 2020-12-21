916b8353b4ca36af9dccf8da9f86f304f4cfe31f
Daniel <admg@ciencias.unam.mx>
2020-12-20 23:13:20 -0600

    Added diff exp with DESeq2

--- COVID_SCRIPT.R
+++ COVID_SCRIPT.R
@@ -1,5 +1,7 @@
 library(GEOquery)
 library(Biobase)
+library(DESeq2)
+library(dplyr)
 
 gset <- getGEO("GSE162113", GSEMatrix =TRUE)
 
@@ -61,3 +63,118 @@ aux_casos <- function(datos, tejido_origen){
 ##NOT RUN
 #test <- aux_control(datos_raton_dia7,"Kidney")
 #test2 <- aux_casos(datos_raton_dia7,"Spleen")
+
+
+####Diff exp between organs
+
+#########Heart
+#########
+#########
+#########
+heart_controles <- aux_control(datos_raton_dia7,"Heart")
+heart_casos <- aux_casos(datos_raton_dia7,"Heart")
+
+heart_counts <- as.matrix(cbind(heart_controles, heart_casos))
+
+condition <- factor(c( rep(c("control","enfermos"), 
+                           c(ncol(heart_controles),
+                             ncol(heart_casos)))))
+coldata <- data.frame(row.names=colnames(heart_counts), condition)
+dds <- DESeqDataSetFromMatrix(countData=heart_counts,
+                              colData=coldata, 
+                              design=~condition)
+dds$condition <- relevel(dds$condition, ref="control")
+dds <- DESeq(dds, parallel = TRUE)
+
+####Obtener todos los resultados y quedarse solo con los significativos
+res_heart <- as.data.frame(results(dds))
+res_heart <- filter(res_heart, padj < 0.05)
+
+####################END HEART
+####################
+####################
+
+
+################Kidney
+controles <- aux_control(datos_raton_dia7,"Kidney")
+casos <- aux_casos(datos_raton_dia7,"Kidney")
+
+counts <- as.matrix(cbind(controles, casos))
+
+condition <- factor(c( rep(c("control","enfermos"), 
+                           c(ncol(controles),
+                             ncol(casos)))))
+coldata <- data.frame(row.names=colnames(counts), condition)
+dds <- DESeqDataSetFromMatrix(countData=counts,
+                              colData=coldata, 
+                              design=~condition)
+dds$condition <- relevel(dds$condition, ref="control")
+dds <- DESeq(dds, parallel = TRUE)
+
+####Obtener todos los resultados y quedarse solo con los significativos
+res <- as.data.frame(results(dds))
+res_kidney <- filter(res, padj < 0.05)
+
+
+####################END Kidney
+####################
+####################
+
+
+################Lung
+controles <- aux_control(datos_raton_dia7,"Lung")
+casos <- aux_casos(datos_raton_dia7,"Lung")
+
+counts <- as.matrix(cbind(controles, casos))
+
+condition <- factor(c( rep(c("control","enfermos"), 
+                           c(ncol(controles),
+                             ncol(casos)))))
+coldata <- data.frame(row.names=colnames(counts), condition)
+dds <- DESeqDataSetFromMatrix(countData=counts,
+                              colData=coldata, 
+                              design=~condition)
+dds$condition <- relevel(dds$condition, ref="control")
+dds <- DESeq(dds, parallel = TRUE)
+
+####Obtener todos los resultados y quedarse solo con los significativos
+res <- as.data.frame(results(dds))
+res_lung <- filter(res, padj < 0.05)
+
+
+####################END Lung
+####################
+####################
+
+################Spleen
+controles <- aux_control(datos_raton_dia7,"Spleen")
+casos <- aux_casos(datos_raton_dia7,"Spleen")
+
+counts <- as.matrix(cbind(controles, casos))
+
+condition <- factor(c( rep(c("control","enfermos"), 
+                           c(ncol(controles),
+                             ncol(casos)))))
+coldata <- data.frame(row.names=colnames(counts), condition)
+dds <- DESeqDataSetFromMatrix(countData=counts,
+                              colData=coldata, 
+                              design=~condition)
+dds$condition <- relevel(dds$condition, ref="control")
+dds <- DESeq(dds, parallel = TRUE)
+
+####Obtener todos los resultados y quedarse solo con los significativos
+res <- as.data.frame(results(dds))
+res_spleen <- filter(res, padj < 0.05)
+
+
+####################END Spleen
+####################
+####################
+
+
+####Guardar resultados de diff exp en csv
+
+write.csv(res_heart,"res_heart.csv")
+write.csv(res_kidney,"res_kidney.csv")
+write.csv(res_lung,"res_lung.csv")
+write.csv(res_spleen,"res_spleen.csv")
